dcce765e7fe88f6b60e3845cf873c511e8ec02be
hirokazukawamuro <110535018+hirokazukawamuro@users.noreply.github.com>
2023-12-20 21:39:00 +0900

    weather_function

--- resources/js/components/Weather.vue
+++ resources/js/components/Weather.vue
@@ -7,15 +7,13 @@ const latitude = ref<number | null>(null);
 const longitude = ref<number | null>(null);
 
 const getWeather = () => {
-  // Convert latitude and longitude to numbers
-
   axios
     .get(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude.value}&longitude=${longitude.value}`
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude.value}&longitude=${longitude.value}&hourly=temperature_2m&daily=weather_code&timezone=Asia%2FTokyo`
     )
     .then((response) => {
       console.log(response.data);
-      // Update the 'weather' ref with the response data
+
       weather.value = response.data;
     })
     .catch((error) => {
@@ -27,7 +25,7 @@ const getWeather = () => {
 <template>
   <main>
     <input v-model="latitude" type="text" id="latitude" placeholder="緯度" />
-    <input v-model="longitude" type="text" id="longitude" placeholder="軽度" />
+    <input v-model="longitude" type="text" id="longitude" placeholder="経度" />
     <button @click="getWeather">Get Weather</button>
     <h1>東京の 1 時間ごとの天気</h1>
 
